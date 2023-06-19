8b7c88e2b92bd6971ceab0f9df96c096d6082200
OPastran02 <opastran02@gmail.com>
2023-06-19 00:31:50 -0300

    Player Finished

--- src/Contexts/Heroes/Hero/Application/LootBoxGenerator.ts
+++ src/Contexts/Heroes/Hero/Application/LootBoxGenerator.ts
@@ -32,21 +32,15 @@ export class LootboxGenerator {
     getRandomPosition(tierProbabilities: number[]): number {
       const totalPositiveProbability = tierProbabilities.reduce((a, b) => a + b, 0);
       const random = this.rng() * totalPositiveProbability;
-      console.log(totalPositiveProbability)
-      console.log(random)
       let cumulativeProbability = 0;
 
       for (let i = tierProbabilities.length-1; i > 0; i--) {
         cumulativeProbability += tierProbabilities[i];
-        console.log(cumulativeProbability);
         if (random < cumulativeProbability) {
-          console.log("llegue a %d", i);
           return i;
         }
       }
-  
-      // En caso de que no se haya seleccionado ninguna posición,
-      // devolver la última posición como fallback
+
       return tierProbabilities.length - 1;
     }
 
